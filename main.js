79dd4b54ef70d1fccf962ab8f7b457fb5e7df599
kirsten larsen <kirstenlarsen02@g.ucla.edu>
2024-04-01 19:16:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text", {
+    Strings:["Frontend Developer", "Youtuber", "Web developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+
+})
